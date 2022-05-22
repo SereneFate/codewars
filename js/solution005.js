ff95d6ba0d43e011057727e7c5b24f20e8a02df4
SereneFate <m7sns7s@hotmail.com>
2022-05-23 02:44:00 +0300

    day 5 done

---
+++ js/solution005.js
@@ -0,0 +1,20 @@
+function getCount(str) {
+    let vowelsCount = 0;
+    let array= str.split('')
+    for(let letter of array){
+      switch(letter){
+          case 'a': vowelsCount+=1
+          break;
+           case 'e': vowelsCount+=1
+          break;
+           case 'i': vowelsCount+=1
+          break;
+           case 'o': vowelsCount+=1
+          break;
+           case 'u': vowelsCount+=1
+          break;
+         
+      }
+    }
+    return vowelsCount;
+  }
